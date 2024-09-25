ad195a85d8e232d8797c04c24b2d234abed43b8e
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-09-25 23:22:59 +0530

    feat: update passport jwt code base

--- backend/configs/passport.js
+++ backend/configs/passport.js
@@ -1,6 +1,15 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('./models/User');
 require("dotenv").config({ path: ".env.local" });
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
 
 passport.use(
   new GoogleStrategy(
@@ -8,10 +17,9 @@ passport.use(
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: "http://localhost:4000/users/google/callback",
-      passReqToCallback: true,  // Set to true if you need the request object in the callback
+      passReqToCallback: true,  
     },
     function (req, accessToken, refreshToken, profile, done) {
-      // Handle the authentication result here
       done(null, profile);  
       console.log("profile", profile);
     }
@@ -25,3 +33,21 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
+
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+module.exports = passport;
